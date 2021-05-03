02a2c5125fba45a61937d6d4df72b7e26aab0f85
Carl Fremault <carlfremault@yahoo.com>
2021-05-03 19:01:06 +0200

    correction

--- github.js
+++ github.js
@@ -1,8 +1,5 @@
 class GitHub {
     constructor() {
-        // this.client_id = '4e8a68f88d6ec13a506a';
-        // this.client_secret = '3f7fbc7ef76cccb73ffe6478a816f7cc180ee424';
-
         this.config = {
             headers: {
                 Authorization: 'token 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'
